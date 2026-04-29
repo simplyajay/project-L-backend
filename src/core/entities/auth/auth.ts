5fb8df5a048570e1903918d926afb34661af731a
Jay <aldriahnjay@LAPTOP-THJJ1QQV.>
2026-04-29 15:02:00 +0400

    refactored folder structure. separated admin and user controllers

--- src/core/entities/auth/auth.ts
+++ src/core/entities/auth/auth.ts
@@ -1,23 +1,24 @@
 import jwt from "jsonwebtoken";
 import env from "@/config/env";
 import { nanoid } from "nanoid";
-import { AppError } from "@/core/types/error";
-import { ISafeUser } from "../user/user";
+import { AppError } from "@/core/utils/error.util";
+import { UserDTOType } from "../user/user";
 import { Types } from "mongoose";
 
-export interface ILoginCredentials {
+export type LoginCredentials = {
   identifier: string;
   loginPassword: string;
-}
+};
 
-export interface IJwtPayload {
+export type JWTPayload = {
   id: Types.ObjectId;
   sessionId: string;
   loggedInAt: number;
   email: string;
-}
+  role: "admin" | "user";
+};
 
-export const generateAccessToken = (user: ISafeUser): string => {
+export const generateAccessToken = (user: UserDTOType, role: "admin" | "user"): string => {
   try {
     const secret = env.get("ACCESS_TOKEN_SECRET");
     const ACCESS_TOKEN_EXPIRY = "30m";
@@ -27,11 +28,12 @@ export const generateAccessToken = (user: ISafeUser): string => {
     const sessionId = nanoid(64);
     const loginTime = Date.now();
 
-    const payload: IJwtPayload = {
+    const payload: JWTPayload = {
       id: user._id,
       sessionId,
       loggedInAt: loginTime,
       email: user.email,
+      role,
     };
     const accessToken = jwt.sign(payload, secret, { expiresIn: ACCESS_TOKEN_EXPIRY });
     //set redis session here. use the userId as the key and sessionId and loginTime as its value
